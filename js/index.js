7a659b5ec7dc2a8166efcc29f8c0705e44534a8e
Anatoliy <andyone4311@gmail.com>
2020-11-26 16:10:59 +0200

    add some vue.js

--- js/index.js
+++ js/index.js
@@ -57,3 +57,78 @@ const app6 = new Vue ({
     }
 });
 
+
+Vue.component('todo-item',{
+    props:['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue ({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Овочі' },
+            { id: 1, text: 'Сир' },
+            { id: 2, text: 'Що ще люди їдять' }
+        ]
+    }
+})
+
+let obj = {
+    foo: 'bar',
+    place: 'IF'
+}
+
+// Object.freeze(obj);
+
+new Vue({
+    el: '#app-8',
+    data: obj
+})
+
+const vm = new Vue ({
+    el: '#example',
+    data: {
+        message: 'Привіт'
+    },
+    computed: {
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('')
+        }
+    }
+})
+
+const watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+        question: '',
+        answer: 'Поки ви не задасте питання, я не можу відповісти!'
+    },
+    watch: {
+        question: function(newQuestion, oldQuestin) {
+            this.answer = 'Очікую, коли ви закінчите друкувати...'
+            this.debouncedGetAnswer()
+        }
+    },
+    created: function() {
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+        getAnswer: function () {
+            if (this.question.indexOf('?') === -1)  {
+                this.answer = 'Запитання зазвичай закінчуються знаком питання.'
+                return
+            }
+            this.answer = 'Думаю...'
+            var vm = this
+            axios.get('https://yesno.wtf/api')
+                .then(function (response) {
+                    vm.answer = _.capitalize(response.data.answer)
+                })
+                .catсh(function (error) {
+                    vm.answer = 'Помилка! Не можу звязатися з API' + error
+                })
+        }
+        
+    }
+})
